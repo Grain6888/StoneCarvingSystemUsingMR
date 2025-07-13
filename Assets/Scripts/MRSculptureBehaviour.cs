7f0cf3d7610e43316b9340609e6762f02d5754a6
Grain6888 <146320184+Grain6888@users.noreply.github.com>
2025-07-13 21:24:43 +0900

    生成範囲を定義

--- Assets/Scripts/MRSculptureBehaviour.cs
+++ Assets/Scripts/MRSculptureBehaviour.cs
@@ -1,16 +1,21 @@
+﻿using Unity.Mathematics;
 using UnityEngine;
 
 public class MRSculptureBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// 彫刻素材の生成範囲
+    /// </summary>
+    public int3 size = new int3(100, 100, 100);
+
+    private void Awake()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
