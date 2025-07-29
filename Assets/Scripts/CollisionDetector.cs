db4103a905673e8828b286db48a6587ad8633f7d
Grain6888 <146320184+Grain6888@users.noreply.github.com>
2025-07-29 15:36:19 +0900

    衝突時の発火を修正
    triggerはfalse
    kinematicはtueにすると衝突判定ができないのでfalse
    同じ理由でkinematicWhileSelectedもfalseに
    このままだとkinematicがfalseのせいで物理演算が働いてオブジェクトが浮遊したり飛んでいったりする
    なのでrigidbodyのconstrainsで勝手に浮遊するのを抑制する

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // 衝突した相手のGameObject名をデバッグログに出力
+        Debug.Log("UNCHI Collision Detected: " + collision.gameObject.name);
+
+        // 衝突時のインパルス（力のベクトル）を取得
+        Vector3 impulse = collision.impulse;
+
+        // インパルスの大きさ（衝撃の強さ）を計算
+        float impactMagnitude = impulse.magnitude;
+
+        // 衝撃の強さをデバッグログに出力
+        Debug.Log("UNCHI Impact Magnitude: " + impactMagnitude);
+
+        // 衝突した全ての接触点について処理
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            // 接触点の座標を取得
+            Vector3 contactPoint = contact.point;
+            // 接触点の法線（面の向き）を取得
+            Vector3 contactNormal = contact.normal;
+
+            // 接触点の座標をデバッグログに出力
+            Debug.Log("UNCHI Contact Point: " + contactPoint);
+            // 接触点の法線をデバッグログに出力
+            Debug.Log("UNCHI Contact Normal: " + contactNormal);
+        }
+    }
+}
--- Assets/Scripts/HandToolsController.cs
+++ Assets/Scripts/HandToolsController.cs
@@ -1,30 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 
 namespace MRSculpture
 {
     public class HandToolsController : MonoBehaviour
     {
-        [SerializeField] private GameObject leftPokeLocation = null;
-        [SerializeField] private GameObject rightPokeLocation = null;
-        [SerializeField] private GameObject chiselPrefab = null;
-        private GameObject chiselInstance = null;
-        private Vector3 chiselPosition = Vector3.zero;
-        private Quaternion chiselRotation = Quaternion.identity;
-        [SerializeField] private GameObject hammerPrefab = null;
-        private GameObject hammerInstance = null;
-        private Vector3 hammerPosition = Vector3.zero;
-        private Quaternion hammerRotation = Quaternion.identity;
-
         void Start()
         {
-            chiselPosition = chiselPrefab.transform.position;
-            chiselRotation = chiselPrefab.transform.rotation;
-            chiselInstance = Instantiate(chiselPrefab, chiselPosition, chiselRotation);
-
-            hammerPosition = hammerPrefab.transform.position;
-            hammerRotation = hammerPrefab.transform.rotation;
-            hammerInstance = Instantiate(hammerPrefab, hammerPosition, hammerRotation);
         }
 
         void Update()
