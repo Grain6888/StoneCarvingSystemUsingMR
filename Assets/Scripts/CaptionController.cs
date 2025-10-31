0be7c3e53951a594a734ee02d078ae7170c41f33
Grain6888 <146320184+Grain6888@users.noreply.github.com>
2025-10-31 21:44:57 +0900

    つかんだ時にキャプションが消えるように変更

--- Assets/Scripts/CaptionController.cs
+++ Assets/Scripts/CaptionController.cs
@@ -5,8 +5,26 @@ public class CaptionController : MonoBehaviour
     [SerializeField]
     private Transform _cameraRig;
 
+    [SerializeField]
+    private Canvas _captionCanvas;
+
+    private void Awake()
+    {
+        _captionCanvas = GetComponent<Canvas>();
+    }
+
     private void Update()
     {
         transform.LookAt(_cameraRig, Vector3.up);
+    }
+
+    public void HideCaption()
+    {
+        _captionCanvas.enabled = false;
+    }
+
+    public void ShowCaption()
+    {
+        _captionCanvas.enabled = true;
     }
 }
