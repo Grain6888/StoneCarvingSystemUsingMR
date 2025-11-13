21f020bab2c8c9e38bf3904fa48fb08d680483f5
Grain6888 <146320184+Grain6888@users.noreply.github.com>
2025-11-13 22:46:37 +0900

    ComputeMarchingCubesからスクリプトを移植(未修整)

--- Assets/Scripts/MainBehaviour.cs
+++ Assets/Scripts/MainBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Unity.Mathematics;
 using UnityEngine;
+using MarchingCubes;
 
 namespace MRSculpture
 {
@@ -9,7 +10,15 @@ namespace MRSculpture
         /// <summary>
         /// 彫刻素材の生成範囲
         /// </summary>
-        public int3 _boundsSize = new(100, 100, 100);
+        public Vector3Int _boundsSize = new(100, 100, 100);
+
+        [SerializeField] float _gridScale = 1.0f;
+        [SerializeField] int _triangleBudget = 65536 * 16;
+        [SerializeField] ComputeShader _builderCompute = null;   // MarchingCubes.compute
+        float _builtTargetValue = 1.0f;
+        int _voxelCount => _boundsSize.x * _boundsSize.y * _boundsSize.z;
+        ComputeBuffer _voxelBuffer;
+        MeshBuilder _builder;
 
         /// <summary>
         /// 彫刻素材のボクセルデータを保存するDataChunk
@@ -53,6 +62,9 @@ namespace MRSculpture
             Matrix4x4 localToWorldMatrix = transform.localToWorldMatrix;
 
             _renderer = new Renderer(_voxelMesh, _voxelMaterial, localToWorldMatrix);
+
+            _voxelBuffer = new ComputeBuffer(_voxelCount, sizeof(float));
+            _builder = new MeshBuilder(_boundsSize, _triangleBudget, _builderCompute);
 
             NewFile();
         }
@@ -98,22 +110,33 @@ namespace MRSculpture
 
         public void NewFile()
         {
-            for (int y = 0; y < _voxelDataChunk.yLength; y++)
-            {
-                DataChunk xzLayer = _voxelDataChunk.GetXZLayer(y);
+            // VoxelBuffer を指定の規則で初期化
+            int xSize = _boundsSize.x;
+            int ySize = _boundsSize.y;
+            int zSize = _boundsSize.z;
+            float[] voxels = new float[_voxelCount];
 
-                for (int x = 0; x < _voxelDataChunk.xLength; x++)
+            for (int y = 0; y < ySize; y++)
+            {
+                for (int z = 0; z < zSize; z++)
                 {
-                    for (int z = 0; z < _voxelDataChunk.zLength; z++)
+                    for (int x = 0; x < xSize; x++)
                     {
-                        int index = xzLayer.GetIndex(x, 0, z);
-                        xzLayer.AddFlag(index, CellFlags.IsFilled);
+                        // 指定のインデックス規則
+                        int index = x + (z * xSize) + (y * xSize * zSize);
+
+                        // 立方体の表面から2マス以内は 0.0、それ以外は 1.0
+                        bool nearSurface = (x < 2) || (x >= xSize - 2) ||
+                                           (y < 2) || (y >= ySize - 2) ||
+                                           (z < 2) || (z >= zSize - 2);
+                        voxels[index] = nearSurface ? 0.0f : 1.0f;
                     }
                 }
-                //_renderer.AddRenderBuffer(xzLayer, y);
             }
 
-            Debug.Log("MRSculpture New DataChunk created.");
+            _voxelBuffer.SetData(voxels);
+
+            Debug.Log("MRSculpture New voxel buffer created.");
 
             _ready = true;
         }
@@ -130,28 +153,32 @@ namespace MRSculpture
             }
             else
             {
+                _builder.BuildIsosurface(_voxelBuffer, _builtTargetValue, _gridScale);
+                GetComponent<MeshFilter>().sharedMesh = _builder.Mesh;
                 rendered = true;
             }
 
-            _impactRange = Mathf.Min(10, (int)(_hammerController.ImpactMagnitude * 5));
+            //_impactRange = Mathf.Min(10, (int)(_hammerController.ImpactMagnitude * 5));
 
-            Vector3 boundingBoxSize = transform.localToWorldMatrix.MultiplyPoint(new Vector3(_boundsSize.x, _boundsSize.y, _boundsSize.z));
-            Bounds boundingBox = new();
-            boundingBox.SetMinMax(Vector3.zero, boundingBoxSize);
+            //Vector3 boundingBoxSize = transform.localToWorldMatrix.MultiplyPoint(new Vector3(_boundsSize.x, _boundsSize.y, _boundsSize.z));
+            //Bounds boundingBox = new();
+            //boundingBox.SetMinMax(Vector3.zero, boundingBoxSize);
 
-            if (_impactRange > 0)
-            {
-                _roundChiselController.Carve(ref _voxelDataChunk, in _impactRange, ref _renderer);
-                _flatChiselController.Carve(ref _voxelDataChunk, in _impactRange, ref _renderer);
-            }
+            //if (_impactRange > 0)
+            //{
+            //    _roundChiselController.Carve(ref _voxelDataChunk, in _impactRange, ref _renderer);
+            //    _flatChiselController.Carve(ref _voxelDataChunk, in _impactRange, ref _renderer);
+            //}
 
-            _renderer.RenderMeshes(new Bounds(boundingBoxSize * 0.5f, boundingBoxSize));
+            //_renderer.RenderMeshes(new Bounds(boundingBoxSize * 0.5f, boundingBoxSize));
         }
 
         private void OnDestroy()
         {
             _renderer.Dispose();
             _voxelDataChunk.Dispose();
+            _voxelBuffer.Dispose();
+            _builder.Dispose();
         }
     }
 }
---
+++ Assets/Scripts/MeshBuilder.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace MarchingCubes
+{
+    //
+    // Marching Cubes Algorithmによる等値面メッシュビルダー
+    //
+    sealed class MeshBuilder : System.IDisposable
+    {
+        #region Public members
+
+        public Mesh Mesh => _mesh;
+
+        public MeshBuilder(int x, int y, int z, int budget, ComputeShader compute)
+          => Initialize((x, y, z), budget, compute);
+
+        public MeshBuilder(Vector3Int dims, int budget, ComputeShader compute)
+          => Initialize((dims.x, dims.y, dims.z), budget, compute);
+
+        public void Dispose()
+          => ReleaseAll();
+
+        public void BuildIsosurface(ComputeBuffer voxels, float target, float scale)
+          => RunCompute(voxels, target, scale);
+
+        #endregion
+
+        #region Private members
+
+        (int x, int y, int z) _grids;
+        int _triangleBudget;
+        ComputeShader _compute;
+
+        void Initialize((int, int, int) dims, int budget, ComputeShader compute)
+        {
+            _grids = dims;
+            _triangleBudget = budget;
+            _compute = compute;
+
+            AllocateBuffers();
+            AllocateMesh(3 * _triangleBudget);
+        }
+
+        void ReleaseAll()
+        {
+            ReleaseBuffers();
+            ReleaseMesh();
+        }
+
+        void RunCompute(ComputeBuffer voxels, float target, float scale)
+        {
+            _counterBuffer.SetCounterValue(0);
+
+            // 等値面再構築
+            _compute.SetInts("Dims", _grids);
+            _compute.SetInt("MaxTriangle", _triangleBudget);
+            _compute.SetFloat("Scale", scale);
+            _compute.SetFloat("Isovalue", target);
+            _compute.SetBuffer(0, "TriangleTable", _triangleTable);
+            _compute.SetBuffer(0, "Voxels", voxels);
+            _compute.SetBuffer(0, "VertexBuffer", _vertexBuffer);
+            _compute.SetBuffer(0, "IndexBuffer", _indexBuffer);
+            _compute.SetBuffer(0, "Counter", _counterBuffer);
+            _compute.DispatchThreads(0, _grids);
+
+            // 未使用領域のクリア
+            _compute.SetBuffer(1, "VertexBuffer", _vertexBuffer);
+            _compute.SetBuffer(1, "IndexBuffer", _indexBuffer);
+            _compute.SetBuffer(1, "Counter", _counterBuffer);
+            _compute.DispatchThreads(1, 1024, 1, 1);
+
+            // バウンディングボックス
+            var ext = new Vector3(_grids.x, _grids.y, _grids.z) * scale;
+            _mesh.bounds = new Bounds(Vector3.zero, ext);
+        }
+
+        #endregion
+
+        #region Compute buffer objects
+
+        ComputeBuffer _triangleTable;
+        ComputeBuffer _counterBuffer;
+
+        void AllocateBuffers()
+        {
+            // マーチングキューブ用トライアングルテーブル
+            _triangleTable = new ComputeBuffer(256, sizeof(ulong));
+            _triangleTable.SetData(PrecalculatedData.TriangleTable);
+
+            // 三角形数カウント用バッファ
+            _counterBuffer = new ComputeBuffer(1, 4, ComputeBufferType.Counter);
+        }
+
+        void ReleaseBuffers()
+        {
+            _triangleTable.Dispose();
+            _counterBuffer.Dispose();
+        }
+
+        #endregion
+
+        #region Mesh objects
+
+        Mesh _mesh;
+        GraphicsBuffer _vertexBuffer;
+        GraphicsBuffer _indexBuffer;
+
+        void AllocateMesh(int vertexCount)
+        {
+            _mesh = new Mesh();
+
+            // GraphicsBuffer を Raw (ByteAddress) バッファとしてアクセス可能にする
+            _mesh.indexBufferTarget |= GraphicsBuffer.Target.Raw;
+            _mesh.vertexBufferTarget |= GraphicsBuffer.Target.Raw;
+
+            // 頂点座標: float32 x 3
+            var vp = new VertexAttributeDescriptor
+              (VertexAttribute.Position, VertexAttributeFormat.Float32, 3);
+
+            // 法線: float32 x 3
+            var vn = new VertexAttributeDescriptor
+              (VertexAttribute.Normal, VertexAttributeFormat.Float32, 3);
+
+            // 頂点/インデックスバッファフォーマット設定
+            _mesh.SetVertexBufferParams(vertexCount, vp, vn);
+            _mesh.SetIndexBufferParams(vertexCount, IndexFormat.UInt32);
+
+            // サブメッシュ初期化
+            _mesh.SetSubMesh(0, new SubMeshDescriptor(0, vertexCount),
+                             MeshUpdateFlags.DontRecalculateBounds);
+
+            // GraphicsBuffer 参照取得
+            _vertexBuffer = _mesh.GetVertexBuffer(0);
+            _indexBuffer = _mesh.GetIndexBuffer();
+        }
+
+        void ReleaseMesh()
+        {
+            _vertexBuffer.Dispose();
+            _indexBuffer.Dispose();
+            Object.Destroy(_mesh);
+        }
+
+        #endregion
+    }
+}
